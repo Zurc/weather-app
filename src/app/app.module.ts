c18de28a6d57836dde96329fd6756598ff7c6cd3
Zurc <cruzjurado@gmail.com>
2019-04-06 15:27:53 +0100

    add some material component modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+
 
 
 import { AppComponent } from './app.component';
@@ -11,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
